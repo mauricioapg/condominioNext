6b2f1cf696eca3efde8e3fb28a28fc12c12fa2e2
Mauricio <mauricioaparecidogabriel@gmail.com>
2021-09-19 19:54:08 -0300

    corrigindo instalacao firebase

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73e983197404336baf4a"; }
+/******/ 		__webpack_require__.h = function() { return "bd219dcdb10d4c97131d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/backend/config.ts
+++ src/backend/config.ts
@@ -3,7 +3,7 @@ import "firebase/firestore"
 
 if(!firebase.apps.length){
     firebase.initializeApp({
-        apiKey: process.env.NEXT_PUBLIC_APIKEY,
+        apiKey: process.env.NEXT_PUBLIC_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
         projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
     })
